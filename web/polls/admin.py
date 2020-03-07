ebdc07773bc651a4f4c271ade4a67fa6b713353d
Nenad Bubalo <48052797+MouseOnTheKeys@users.noreply.github.com>
2020-03-07 20:07:45 +0100

    small update 6th part

--- web/polls/admin.py
+++ web/polls/admin.py
@@ -4,4 +4,9 @@ from .models import Question
 
 # Register your models here.
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
 admin.site.register(Question)
