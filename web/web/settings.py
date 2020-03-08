01faa829a69a09cff522862c5df436fbae225552
Nenad Bubalo <48052797+MouseOnTheKeys@users.noreply.github.com>
2020-03-08 22:34:19 +0100

    end of tutorial 7th part

--- web/web/settings.py
+++ web/web/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "web.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
-
+# Username to Admin: MouseOnTheKeys Pass: MOTK2055wewe
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
